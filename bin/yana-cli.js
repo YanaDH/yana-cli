92524323f570107735949c2de21f4ceff02ee062
duhuan <duhuan@xinshiyun.com>
2022-06-29 16:53:29 +0800

    update package.json

--- bin/yana-cli.js
+++ bin/yana-cli.js
@@ -9,28 +9,28 @@ program
     .command("create")
     .description("创建一个模板项目")
     .action( () =>{
-        require("../src/create")
+        require("../lib/create")
     })
 
 program
     .command("list")
     .description("列出当前可用模板项目")
     .action(()=>{
-        require('../src/list')
+        require('../lib/list')
     })
 
 program
     .command('add')
     .description('添加模板')
     .action(()=>{
-        require('../src/add')
+        require('../lib/add')
     })
 
 program
     .command('delete')
     .description('删除模板')
     .action(()=>{
-        require('../src/delete')
+        require('../lib/delete')
     })
     
 // 解析参数
